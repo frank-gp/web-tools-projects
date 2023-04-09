bad804800ba04cfa684c1e87a2414d7fae963217
Thinkpad <fgp555@gmail.com>
2023-04-09 06:03:32 -0500

    02 emailjs

---
+++ git-add-amend-push--force-start.sh
@@ -0,0 +1,32 @@
+read -p "Press 2 [Enter] key to add amend no-edit..."
+read -p "Press 1 [Enter] key to add amend no-edit..."
+
+echo ""
+echo "==================="
+echo "git add . && git commit --amend --no-edit && git push --force"
+echo "==================="
+git add . && git commit --amend --no-edit && git push --force
+
+echo ""
+echo "==================="
+echo "git status..."
+echo "==================="
+git status
+
+echo ""
+echo "==================="
+echo "git log --oneline..."
+echo "==================="
+git log --oneline
+
+echo ""
+gbrowsevar=$(git config --get remote.origin.url)
+printf "${gbrowsevar}"
+start $gbrowsevar
+echo ""
+
+echo ""
+git config --get remote.origin.url
+
+
+read -t 30 -p "Press [Enter] key to exit..."
---
+++ git-status--oneline.sh
@@ -0,0 +1,26 @@
+echo ""
+echo ""
+echo "======================================"
+echo "git status..."
+echo "======================================"
+git status
+
+echo ""
+echo ""
+echo "======================================"
+echo "git log --oneline..."
+echo "======================================"
+git log --oneline
+
+echo ""
+echo ""
+echo "======================================"
+echo "git config --get remote.origin.url"
+echo "======================================"
+git config --get remote.origin.url
+
+echo ""
+echo ""
+echo ""
+echo ""
+read -t 30 -p "Press [Enter] key to exit..."
